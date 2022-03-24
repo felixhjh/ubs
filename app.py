199128b83877a771be89158e6c700e2a08cde805
felixhjh <852142024@qq.com>
2022-03-24 17:14:42 +0800

    change port

--- app.py
+++ app.py
@@ -983,4 +983,4 @@ def page_not_found(error):
 port = int(os.getenv("PORT", "3000"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
